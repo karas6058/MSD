90088ce5d2370fc5592c92c148964621351369a2
WonHo Bark <kairas@outlook.kr>
2016-11-20 15:55:01 +0900

    add .xml and .java from hw05

---
+++ HW09/app/src/main/java/org/iptime/kairas/hw09/Memo.java
@@ -0,0 +1,12 @@
+package org.iptime.kairas.hw09;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class Memo extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.memo);
+    }
+}
