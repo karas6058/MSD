324500b98c30146e1664e23790acf5960ab73fec
WonHo Bark <kairas@outlook.kr>
2016-11-20 18:55:42 +0900

    make DBHelper.java split

--- HW09/app/src/main/java/org/iptime/kairas/hw09/Phonebook.java
+++ HW09/app/src/main/java/org/iptime/kairas/hw09/Phonebook.java
@@ -93,19 +93,3 @@ public class Phonebook extends AppCompatActivity {
 
 }
 
-class DBHelper extends SQLiteOpenHelper {
-    private static final String DATABASE_NAME = "mycontacts.db";
-    private static final int DATABASE_VERSION = 2;
-
-    public DBHelper(Context context) { super(context, DATABASE_NAME, null, DATABASE_VERSION); }
-
-    public void onCreate(SQLiteDatabase db) {
-        db.execSQL("CREATE TABLE contacts ( _id INTEGER PRIMARY KEY" + " AUTOINCREMENT, name TEXT, tel TEXT);");
-    }
-
-    @Override
-    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        db.execSQL("DROP TABLE IF EXISTS contacts");
-        onCreate(db);
-    }
-}
