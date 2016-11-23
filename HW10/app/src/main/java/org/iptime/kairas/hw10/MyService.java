8cb91a80d50117c1a357289c36664d32bec16eff
WonHo Bark <kairas@outlook.kr>
2016-11-23 15:38:08 +0900

    Rename to HW10

---
+++ HW10/app/src/main/java/org/iptime/kairas/hw10/MyService.java
@@ -0,0 +1,44 @@
+package org.iptime.kairas.hw10;
+
+import android.app.Service;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.IBinder;
+import android.util.Log;
+
+public class MyService extends Service {
+    private MediaPlayer mediaPlayer = null;
+
+    @Override
+    public void onStart(Intent intent, int startId) {
+        super.onStart(intent, startId);
+        Log.d("slog", "onStart()");
+
+        if (mediaPlayer != null)
+            mediaPlayer.stop();
+
+        String file = intent.getStringExtra("file");
+
+        if (file == null)
+            mediaPlayer = MediaPlayer.create(this, R.raw.music1);
+        else {
+            Uri song = Uri.parse(file);
+            mediaPlayer = MediaPlayer.create(this, song);
+        }
+
+        mediaPlayer.start();
+    }
+
+    @Override
+    public void onDestroy() {
+        Log.d("slog", "onDestroy()");
+        mediaPlayer.stop();
+        super.onDestroy();
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+}
