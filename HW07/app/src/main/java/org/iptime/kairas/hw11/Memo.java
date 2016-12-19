3ed13761051c567a6449e08ecdeecad34326863c
WonHo Bark <wonho.park83@gmail.com>
2016-12-19 20:28:11 +0900

    hw06

---
+++ HW07/app/src/main/java/org/iptime/kairas/hw11/Memo.java
@@ -0,0 +1,12 @@
+package org.iptime.kairas.hw11;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Memo extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.memo);
+    }
+}
